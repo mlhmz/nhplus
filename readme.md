5e3b265b8a957a9f0ebda1cc27cb478e8c0e4937
Soeren2208 <s.schwerk@schule.bremen.de>
2020-08-31 16:05:42 +0200

    init

---
+++ readme.md
@@ -0,0 +1,10 @@
+# NHPlus
+
+TODO:  Kurz-Beschreibung der Lernsituation 
+
+
+
+
+### Hinweise
+- Falls im Database-Tab keine Verbindung zu `nursingHomeDB`  aufgebaut werden kann -> Rechtsklick auf `nursingHomeDB` und unter Properties die Treiber herunter laden.
+- Falls die Datenbank bereits im Database-Tab geöffnet ist, kann NHPlus  keinen Zugriff auf die Daten erlangen. Die Verbindung zu `nursingHomeDB` im Database-Tab muss also zunächst geschlossen werden!
---
+++ src/main/java/datastorage/DAOimp.java
@@ -0,0 +1,64 @@
+package datastorage;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class DAOimp<T> {
+    protected Connection conn;
+
+    public DAOimp(Connection conn) {
+        this.conn = conn;
+    }
+
+    public void create(T t) throws SQLException {
+        Statement st = conn.createStatement();
+        st.executeUpdate(getCreateStatementString(t));
+    }
+
+    public T read(int key) throws SQLException {
+        T object = null;
+        Statement st = conn.createStatement();
+        ResultSet result = st.executeQuery(getReadByIDStatementString(key));
+        if (result.next()) {
+            object = getInstanceFromResultSet(result);
+        }
+        return object;
+    }
+
+    public List<T> readAll() throws SQLException {
+        ArrayList<T> list = new ArrayList<T>();
+        T object = null;
+        Statement st = conn.createStatement();
+        ResultSet result = st.executeQuery(getReadAllStatementString());
+        list = getListFromResultSet(result);
+        return list;
+    }
+
+    public void update(T t) throws SQLException {
+        Statement st = conn.createStatement();
+        st.executeUpdate(getUpdateStatementString(t));
+    }
+
+    public void deleteById(int key) throws SQLException {
+        Statement st = conn.createStatement();
+        st.executeUpdate(getDeleteStatementString(key));
+    }
+
+    protected abstract String getCreateStatementString(T t);
+
+    protected abstract String getReadByIDStatementString(int key);
+
+    protected abstract T getInstanceFromResultSet(ResultSet set) throws SQLException;
+
+    protected abstract String getReadAllStatementString();
+
+    protected abstract ArrayList<T> getListFromResultSet(ResultSet set) throws SQLException;
+
+    protected abstract String getUpdateStatementString(T t);
+
+    protected abstract String getDeleteStatementString(int key);
+}

b88ad9a96c13ab62cf6c7108133042c0385d422c
Malek Hamza <malek.hamza@gmx.de>
2022-06-11 00:48:13 +0200

    #2 Script zum Initialisieren der Datenbank hinzugefügt

---
+++ scripts/init_hsql_db.sql
@@ -0,0 +1,30 @@
+-- Patient Table Creation
+CREATE TABLE IF NOT EXISTS patients (
+    pid INT IDENTITY PRIMARY KEY,
+    firstname VARCHAR(60),
+    surname VARCHAR(60),
+    date_of_birth DATE,
+    carelevel SMALLINT,
+    roomNumber SMALLINT
+);
+
+-- Treatment Table Creation
+CREATE TABLE IF NOT EXISTS treatments (
+    tid INT IDENTITY PRIMARY KEY,
+    pid INT,
+    treatment_date DATE,
+    "begin" VARCHAR(10),
+    "end" VARCHAR(10),
+    description VARCHAR(200),
+    remarks VARCHAR(200)
+);
+
+-- User Table Creation
+CREATE TABLE IF NOT EXISTS users (
+    uid INT IDENTITY PRIMARY KEY,
+    firstname VARCHAR(60),
+    lastname VARCHAR(60),
+    username VARCHAR(30) UNIQUE,
+    password VARCHAR(255),
+    group VARCHAR(30)
+)

d4971beccb5dee600199d1cee1b225c5a12f03a0
Malek Hamza <malek.hamza@gmx.de>
2022-06-14 14:55:25 +0200

    #2 PermissionKey Enum dokumentiert und toString() eingebaut

--- src/main/java/enums/PermissionKey.java
+++ src/main/java/enums/PermissionKey.java
@@ -1,21 +1,66 @@
 package enums;
 
 /**
- * Enum for Group Permissions
+ * Enum for the Controller-View-Access, is assigned to Groups {@link model.Group}
  */
 public enum PermissionKey {
-    SHOW_HOMEPAGE,
-    SHOW_ALL_PATIENTS,
-    CREATE_PATIENT,
-    EDIT_PATIENT,
-    DELETE_PATIENT,
-    SHOW_ALL_USERS,
-    CREATE_USER,
-    EDIT_USER,
-    DELETE_USER,
-    SHOW_ALL_TREATMENTS,
-    CREATE_TREATMENT,
-    EDIT_TREATMENT,
-    DELETE_TREATMENT,
+    /**
+     * The MainWindow of the Application (see {@link controller.MainWindowController}),
+     * usually, every group should recieve this permission
+     */
+    SHOW_HOMEPAGE("Startseite zeigen"),
+    /**
+     * Permission to open the Patient Table (see {@link controller.AllPatientController})
+     */
+    SHOW_ALL_PATIENTS("Patienten zeigen"),
+    /**
+     * Permission to the Patient Creation in the {@link controller.AllPatientController}
+     */
+    CREATE_PATIENT("Patient erstellen"),
+    /**
+     * Permission to the Patient Editing in the {@link controller.AllPatientController}
+     */
+    EDIT_PATIENT("Patient editieren"),
+    /**
+     * Permission to the Patient Deletion in the {@link controller.AllPatientController}
+     */
+    DELETE_PATIENT("Patient löschen"),
+    /**
+     * Permission to the User Table (see {@link controller.AllUserController})
+     */
+    SHOW_ALL_USERS("Nutzer zeigen"),
+    /**
+     * Permission to the User Creation in the {@link controller.AllUserController}
+     */
+    CREATE_USER("Nutzer erstellen"),
+    /**
+     * Permission to the User Editing in the {@link controller.AllUserController}
+     */
+    EDIT_USER("Nutzer editieren"),
+    /**
+     * Permission to the User Deletion in the {@link controller.AllUserController}
+     */
+    DELETE_USER("Nutzer löschen"),
+    /**
+     * Permission to the Treatment Table (see {@link controller.AllTreatmentController})
+     */
+    SHOW_ALL_TREATMENTS("Behandlungen zeigen"),
+    /**
+     * Permission to the Treatment Creation (see {@link controller.NewTreatmentController})
+     */
+    CREATE_TREATMENT("Behandlung erstellen"),
+    /**
+     * Permission to the Treatment Editing (see {@link controller.TreatmentController})
+     */
+    EDIT_TREATMENT("Behandlung editieren"),
+    /**
+     * Permission to the Treatment Deletion in the {@link controller.AllTreatmentController}
+     */
+    DELETE_TREATMENT("Behandlung löschen");
 
+    public final String description;
+
+    PermissionKey(String description) {
+        this.description = description;
+    }
 }
